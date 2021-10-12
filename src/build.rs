c2c67b7e6ee8162d9c323ac5ec98f45b7b1f3c86
takashiidobe <idobetakashi@gmail.com>
2021-10-11 20:05:40 -0500

    initial commit

---
+++ src/build.rs
@@ -0,0 +1,15 @@
+use man::prelude::*;
+use std::fs::File;
+use std::io::{Error, Write};
+
+fn main() -> Result<(), Error> {
+    let path = "min-stack.1";
+    let mut output = File::create(path)?;
+
+    let msg = Manual::new("MinStack")
+        .about("A Min Stack in Rust.")
+        .author(Author::new("Takashi I").email("mail@takashiidobe.com"))
+        .render();
+
+    write!(output, "{}", msg)
+}
